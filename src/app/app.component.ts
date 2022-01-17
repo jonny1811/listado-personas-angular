df0b19fd29ed4df85a8cb0f4b42934d8188b353d
jonny1811 <jonny@quanto.la>
2022-01-17 01:21:08 -0300

    chore: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'listado-personas';
+  title = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Juanez'),
+    new Persona('Carlos', 'Lara')
+  ];
+  nameInput: string = '';
+  lastnameInput: string = '';
+
+  addPeople(): void {
+    let newPeople = new Persona(this.nameInput, this.lastnameInput);
+    this.personas.push(newPeople);
+    this.cleanInput();
+  }
+
+  cleanInput(): void {
+    this.nameInput = '';
+    this.lastnameInput = '';
+  }
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona {
+  /*
+  name: string = '';
+  lastName: string = '';
+
+  constructor(name: string, lastname: string) {
+    this.name = name;
+    this.lastName = lastname
+  }
+  */
+
+  constructor(public name: string, public lastname: string) { }
+}
