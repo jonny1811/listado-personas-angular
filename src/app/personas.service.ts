ac152283ed5086bdc42fd807fa316c892f3b7e3d
jonny1811 <jonny@quanto.la>
2022-01-31 00:51:17 -0300

    feat: add all crud method with firabase

--- src/app/personas.service.ts
+++ src/app/personas.service.ts
@@ -5,41 +5,54 @@ import { Persona } from "./persona.model";
 
 @Injectable()
 export class PersonasService {
-  personas: Persona[] = [];
+	personas: Persona[] = [];
 
-  greeting = new EventEmitter<number>();
+	greeting = new EventEmitter<number>();
 
-  constructor(
-    private loggingService: LoggingService,
-    private dataServices: DataServices
-  ) { }
+	constructor(
+		private loggingService: LoggingService,
+		private dataServices: DataServices
+	) { }
 
-  setPeople(peoples: Persona[]) {
-    this.personas = peoples;
-  }
+	setPeople(peoples: Persona[]) {
+		this.personas = peoples;
+	}
 
-  getPeoples() {
-    return this.dataServices.loadPeoples();
-  }
+	getPeoples() {
+		return this.dataServices.loadPeoples();
+	}
 
-  addPeople(persona: Persona): void {
-    this.loggingService.sendMessageConsole('agregamos persona:' + JSON.stringify(persona));
-    this.personas.push(persona);
-    this.dataServices.savePeoples(this.personas);
-  }
+	addPeople(persona: Persona): void {
+		this.loggingService.sendMessageConsole('agregamos persona:' + JSON.stringify(persona));
+		if (this.personas == null) {
+			this.personas = [];
+		}
+		this.personas.push(persona);
+		this.dataServices.savePeoples(this.personas);
+	}
 
-  findPeople(index: number): Persona {
-    let persona: Persona = this.personas[index];
-    return persona;
-  }
+	findPeople(index: number): Persona {
+		let persona: Persona = this.personas[index];
+		return persona;
+	}
 
-  updatePeople(index: number, people: Persona): void {
-    let persona = this.personas[index];
-    persona.name = people.name;
-    persona.lastname = people.lastname;
-  }
+	updatePeople(index: number, people: Persona): void {
+		let persona = this.personas[index];
+		persona.name = people.name;
+		persona.lastname = people.lastname;
+		this.dataServices.updatePeople(index, persona);
+	}
 
-  deletePeople(index: number): void {
-    this.personas.splice(index, 1);
-  }
+	deletePeople(index: number): void {
+		this.personas.splice(index, 1);
+		this.dataServices.deletePeople(index);
+		// se vuelve a guardar el arreglo para regenerar los indices
+		this.modificarPersonas();
+	}
+
+	modificarPersonas() {
+		if (this.personas != null) {
+			this.dataServices.savePeoples(this.personas);
+		}
+	}
 }
